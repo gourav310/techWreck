418605bbe9a44d203c0d9af63640528fa3118321
gourav310 <67389253+gourav310@users.noreply.github.com>
2021-01-31 23:50:30 +0530

    Update Partner.js

--- src/components/Partner.js
+++ src/components/Partner.js
@@ -11,7 +11,7 @@ export default function App(){
     return(<div className="conatiner-md partnerCol" >
       <div className="row partnerRow" ><h3 >Become a Service Partner</h3></div>
       <div className="row partnerRow"><p style={{color:"6d7d8c"}}>Earn up to 100<span>&#8377;</span>/hr for helping people discover and enjoy technology.</p></div>
-      <div className="row partnerRow" ><Button color="trasparent" style={{border:"1px solid #236084"}}>Know More</Button></div>
+       <div className="row partnerRow" ><Button color="trasparent" style={{border:"1px solid #236084"}}><a rel="noreferrer" href="https://techpartner0.herokuapp.com/" target="_Blank">Know More</a></Button></div>
   
     </div>)
 }
